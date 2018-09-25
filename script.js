6ec06afdd09c878ba5a9222b8700076635bb40c1
Voodoobear <s.chalain@gmail.com>
2018-09-25 14:42:44 +0200

    over! everything seems to work as expected

--- script.js
+++ script.js
@@ -3,33 +3,46 @@ $(function() {
 
     let $mainMenuItems = $('#main-menu ul').children('li'),
     totalMainMenuItems = $mainMenuItems.length,
-    openedIndex = -1,
+    openedIndex = 2,
 
+    //function that will initiate the animation part with images
     init = () => {
+        bindEvents();
 
+        if(validIndex(openedIndex))
+        {
+            animateItem($mainMenuItems.eq(openedIndex), true, 800);
+        }
+        
+    },
+
+    bindEvents = function() {
+        
         $mainMenuItems.children(".images").click(function() {
             
-            let newIndex = $(this).parent().index(),
-            $item = $mainMenuItems.eq(newIndex);
+            let newIndex = $(this).parent().index();
+            checkAndAnimateItem(newIndex);
+        });
 
-            if(openedIndex === newIndex) {
-                animateItem($item, false, speed);
-                openedIndex = -1;
-            } else {
+        $('.button').hover(function() {
+            $(this).addClass("hovered");
+        },
+        function() {
+            $(this).removeClass("hovered");
+        });
 
-                if (validIndex) {
-                    animateItem($mainMenuItems.eq(openedIndex), false, 350);
-                    openedIndex = newIndex;
-                    animateItem($item, true, 350);
-                }
-            }
+        $('.button').click(function() {
+            let newIndex = $(this).index();
+            checkAndAnimateItem(newIndex);
         });
     },
 
+    //this is to ensure we use the right indexes
     validIndex = function(indexToCheck) {
         return (indexToCheck >= 0) && ( indexToCheck <= totalMainMenuItems);
     },
 
+    //those are to describe steps of the animations
     animateItem = function($item, toOpen, speed) {
 
             let $colorImage = $item.find(".color"),
@@ -38,6 +51,20 @@ $(function() {
             $colorImage.animate(colorImagesParam, speed);
             $item.animate(itemParam, speed);
 
+    },
+
+    checkAndAnimateItem = function(indexToCheckAndAnimate) {
+        if(openedIndex === indexToCheckAndAnimate) {
+            animateItem($mainMenuItems.eq(indexToCheckAndAnimate), false, 350);
+            openedIndex = -1;
+        } else {
+
+            if (validIndex(indexToCheckAndAnimate)) {
+                animateItem($mainMenuItems.eq(openedIndex), false, 350);
+                openedIndex = indexToCheckAndAnimate;
+                animateItem($mainMenuItems.eq(openedIndex), true, 350);
+            }
+        }
     };
     
     init();
